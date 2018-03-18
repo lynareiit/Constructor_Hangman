f0fb85e8f42e0b2520db4d195e586b7a2ee4a0e3
{Alyna} <{lynareiit@gmail.com}>
2018-03-17 21:26:30 -0700

    final push

--- hangman.js
+++ hangman.js
@@ -1,9 +1,11 @@
 var inquirer = require("inquirer");
-
+// Use chalk if there's time for styling in Terminal
+var chalk = require("chalk");
 var currentword = require("./word.js");
 var letter = require('./letter.js');
 
 function startGame() {
+    
     console.log("Hangtight!");
     guessesRemaining = 10;
     answerspaces = [];
